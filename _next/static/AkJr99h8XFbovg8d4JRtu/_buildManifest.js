6676c558e8357df55069cd95f7f512171034f3bc
ninjagrad <37491726+ninjagrad@users.noreply.github.com>
2026-05-29 07:49:45 +0900

    Deploying to gh-pages from @ ninjagrad/wedding-invitation@e3a521e2ccbb7298553c213c60feac8e498f73e5 🚀

--- _next/static/AkJr99h8XFbovg8d4JRtu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/7491423f9a145549.css","static/chunks/pages/index-d289b48a4deef7a5.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
